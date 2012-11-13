7ec05f022a35ca5fa1853d132576125d830eb40d
unknown <Imam@Imam-PC.(none)>
2012-11-13 13:05:16 +0700

    prak5

--- Praktikum/prak5/Main.java
+++
@@ -1,8 +0,0 @@
-import praktikum.java.basic.classpath.Belajar;
-public class Main{
-	public static void main(String[] args){
-		Belajar p = new Belajar();
-		p.setNama("ini nama belajar");
-		System.out.println(p.getNama());
-	}
-}
