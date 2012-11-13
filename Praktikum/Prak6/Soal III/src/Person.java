2c301d8b28fa16e0ed90465061da5f56f61b0f8e
unknown <Imam@Imam-PC.(none)>
2012-11-13 20:27:38 +0700

    pesan

---
+++ Praktikum/Prak6/Soal III/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+	public static void main (String[] args) {
+		int outfirstnumber = 10;
+		double outfirstdouble = 10;
+		
+		System.out.println(outfirstnumber);
+		System.out.println(outfirstdouble);
+	}
+}
